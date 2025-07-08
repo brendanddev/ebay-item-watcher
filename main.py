3534ed230fe75f821359aea05ac4b5abfab86469
Brendan Dileo <dileob23@gmail.com>
2025-07-08 09:37:06 -0400

    Test message sending logic

--- main.py
+++ main.py
@@ -8,17 +8,22 @@ Brendan Dileo - July 2025
 
 from client.auth import get_app_access_token
 from client.ebay_api import search
+from client.notifier import send_telegram_message
 
 
 if __name__ == "__main__":
-    # Get and print the app access token
-    token = get_app_access_token()
-    print("Access Token:", token)
+    send_telegram_message("ebay Item Watcher started successfully!")
     
-    # Perform a test search
-    query = "laptop"
-    results = search(query)
-    if results:
-        print("Search Results:", results)
-    else:
-        print("No results found.")
+    # # Get and print the app access token
+    # token = get_app_access_token()
+    # print("Access Token:", token)
+    
+    # # Perform a test search
+    # query = "laptop"
+    # results = search(query)
+    # if results:
+    #     print("Search Results:", results)
+    # else:
+    #     print("No results found.")
+        
+    
