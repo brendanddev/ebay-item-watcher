a780103f68d1eee985e29fa052c3b219d8559bc8
Brendan Dileo <dileob23@gmail.com>
2025-07-08 11:12:58 -0400

    Update main and workflow

--- main.py
+++ main.py
@@ -12,18 +12,20 @@ from client.notifier import send_telegram_message
 
 
 if __name__ == "__main__":
+    
+    # Sends a message to the user via the Telegram bot
     send_telegram_message("ebay Item Watcher started successfully!")
     
-    # # Get and print the app access token
-    # token = get_app_access_token()
-    # print("Access Token:", token)
+    # Get and print the app access token
+    token = get_app_access_token()
+    print("Access Token:", token)
     
-    # # Perform a test search
-    # query = "laptop"
-    # results = search(query)
-    # if results:
-    #     print("Search Results:", results)
-    # else:
-    #     print("No results found.")
+    # Perform a test search
+    query = "laptop"
+    results = search(query)
+    if results:
+        print("Search Results:", results)
+    else:
+        print("No results found.")
         
     
