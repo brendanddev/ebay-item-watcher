ba47079410602f159d714fb34b2f8738bc46a20e
Brendan Dileo <dileob23@gmail.com>
2025-07-08 16:33:29 -0400

    Update main with new search

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ Brendan Dileo - July 2025
 from client.auth import get_app_access_token
 from client.ebay_api import build_search_params, search
 from client.notifier import send_telegram_message
+from client.formatter import format_search_results
 
 
 if __name__ == "__main__":
@@ -22,13 +23,23 @@ if __name__ == "__main__":
     
     # Build search params with filters
     params = build_search_params(
-        keyword="iPhone",
-        limit=10
+        keyword="Marvel Super Heroes Secret Wars",
+        price_min=5,
+        price_max=100,
+        price_currency="CAD",
+        pickup_postal_code="L6L5W2",
+        pickup_radius=1,
+        item_location_region="ON",
+        item_location_country="CA",
+        limit=15
     )
+
     
     # Perform the search
     results = search(params)
     if results:
-        print("Search Results:", results)
+        formatted_results = format_search_results(results)
+        print("Formatted Results:\n", formatted_results)
+        send_telegram_message(formatted_results)
     else:
         print("No results found or search failed.")
