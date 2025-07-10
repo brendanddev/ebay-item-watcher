5f3f395517f3e1c90db43765e4576d50ba2e1320
Brendan Dileo <dileob23@gmail.com>
2025-07-10 18:52:20 -0400

    Test cron in WSL

--- scripts/setup_cron.sh
+++ scripts/setup_cron.sh
@@ -3,6 +3,4 @@
 # This scripts setus up a cron job to let the user run
 # the ebay item watcher script at a specified interval
 
-APP_PATH=""
-
-(crontab)
+echo "Hello from cron! The time is: $(date)" >> ~/cron_test.log
