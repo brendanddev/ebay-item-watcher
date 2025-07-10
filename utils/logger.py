92be98699b6e42fede00d49297f4357ad9a07efb
Brendan Dileo <dileob23@gmail.com>
2025-07-09 20:04:28 -0400

    Remove previous logging logic from logger class

--- utils/logger.py
+++ utils/logger.py
@@ -8,14 +8,6 @@ Brendan Dileo - July 2025
 
 import sys
 import logging
-import cachetools
-
-try:
-    from colorama import Fore, Style, init as colorama_init
-    colorama_init()
-except ImportError:
-    Fore = Style = None
-
 
 class Logger:
     
@@ -30,7 +22,24 @@ class Logger:
         file_name="ebay_watcher.log",
         timestamp_format="%Y-%m-%d %H:%M:%S",
     ):
-        pass
+        
+        # Set instance vars and prevent duplicate loggers
+        self.logger = logging.getLogger(name)
+        self.logger.setLevel(level)
+        # stop log mesages from being passed to parent loggers
+        self.logger.propagate = False
+        
+        # If no handlers are attached to the logger instance
+        # conditionally add console or file handler
+        if not self.logger.handlers:
+            
+            formatter = logging.Formatter(
+                f"%(asctime)s - {self._colorize(name, name_color)} - %(levelname)s - %(message)s",
+                datefmt=timestamp_format,
+            )
+            
+            if to_console:
+            if to_file:
     
     # Get current instance of the logger
     def get_logger(self):
