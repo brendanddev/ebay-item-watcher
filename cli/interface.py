0cc75ce7e978a8a7a6c14ad8ae84cea2e270c12e
Brendan Dileo <dileob23@gmail.com>
2025-07-10 17:59:43 -0400

    Update comments for cli interface

--- cli/interface.py
+++ cli/interface.py
@@ -12,15 +12,25 @@ from utils.logger import Logger
 logger = Logger().get_logger()
 
 def run_cli():
+    
+    # Creates an instance of ArgumentParser object to handle cli args
     parser = argparse.ArgumentParser(description="eBay Item Watcher CLI")
+    
+    # Accepts a cli argument for the command to run
     parser.add_argument(
         "command",
         nargs="?",
         choices=["search"],
         help="Choose a command (or leave blank for menu)",
     )
+    
+    # Parse cli args into structured object
+    logger.info("Parsing command line arguments...")
     args = parser.parse_args()
 
+    logger.info("Parsed command line arguments...")
+    
+    # If a user has provided a command
     if args.command:
         logger.info(f"Running command: {args.command}")
         if args.command == "search":
