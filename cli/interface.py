9e33c2e314ac6f8d6365b5b3dcc94ffddbf639f1
Brendan Dileo <dileob23@gmail.com>
2025-07-10 20:09:33 -0400

    Update interface to use ui

--- cli/interface.py
+++ cli/interface.py
@@ -6,29 +6,14 @@ Brendan Dileo - July 2025
 """
 
 import argparse
-from time import sleep
 from cli.commands import perform_search
+from cli.ui import show_main_menu, prompt_for_notification, loading_spinner
 from utils.logger import Logger
-from rich.console import Console
-from rich.spinner import Spinner
-from InquirerPy import inquirer
 
 logger = Logger().get_logger()
-console = Console()
-    
 
-# Prompts the user for notification method to use
-def prompt_for_notification():
-    print("Choose notification method(s):")
-    print("1. Telegram Bot only")
-    print("2. Email only")
-    print("3. Both Telegram Bot and Email")
-    print("4. None (just display results)")
-    choice = input("Enter your choice (1-4): ").strip()
-    return choice 
 
 def run_cli():
-    
     # Creates an instance of ArgumentParser object to handle cli args
     parser = argparse.ArgumentParser(description="eBay Item Watcher CLI")
     
@@ -43,7 +28,6 @@ def run_cli():
     # Parse cli args into structured object
     logger.info("Parsing command line arguments...")
     args = parser.parse_args()
-
     logger.info("Parsed command line arguments...")
     
     # If a user has provided a command
@@ -52,19 +36,16 @@ def run_cli():
         if args.command == "search":
             perform_search()
     else:
-        
-        # App based menu if no cli args
-        print("Welcome to eBay Item Watcher!")
-        print("Available commands:")
-        print("1. Search")
-
         logger.info("Prompting user for menu choice")
-        choice = input("Enter a command number: ").strip()
-        logger.debug(f"User selected menu option: {choice}")
+        
+        # cli menu if no args provided
+        choice = show_main_menu()
 
-        if choice == "1":
+        if choice == "Search items":
             notification_choice = prompt_for_notification()
             logger.info("Running 'search' via menu selection")
             perform_search(notification_choice=notification_choice)
-        else:
-            logger.warning("Invalid command entered.")
+            loading_spinner("Searching eBay...")
+        elif choice == "Exit":
+            print("Goodbye!")
+            exit()
